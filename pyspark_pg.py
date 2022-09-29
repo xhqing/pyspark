1ea83f0b7e81852e9d0b5a85f7b80e3458761b5c
xuhuaqing <huaqingxu89@gmail.com>
2022-09-29 11:15:37 +0800

    update

--- pyspark_pg.py
+++ pyspark_pg.py
@@ -1,9 +1,14 @@
 from pyspark.sql import SparkSession
 
+
+sparkClassPath: str = "pyspark/jars/postgresql-42.4.0.jar"
+spark = SparkSession.builder.appName("pyspark-pg").master("local").config("spark.driver.extraClassPath", sparkClassPath).getOrCreate()
+
 class PySparkPG:
     """pyspark connect pg"""
 
     def __init__(self, host: str, port: str, username: str, password: str, dbname: str):
+
         self.dbname = dbname
         self.username = username
         self.password = password
@@ -11,8 +16,6 @@ class PySparkPG:
         self.port = port
 
     def get_sparkDataFrame(self, tablename: str):
-        sparkClassPath = "pyspark/jars/postgresql-42.4.0.jar"
-        spark = SparkSession.builder.appName("pyspark pg app").master("local").config("spark.driver.extraClassPath", sparkClassPath).getOrCreate()
 
         sparkDataFrame = spark.read.format("jdbc").option("url", f"jdbc:postgresql://{self.host}:{self.port}/{self.dbname}").option("driver", "org.postgresql.Driver")\
             .option("dbtable", tablename).option("user", self.username).option("password", self.password).load()
